e2fde42e349d2af910caa364209bbbbf5698697c
Flouse <flouse@gmail.com>
2021-04-18 17:19:12 +0800

    refactor: seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,8 +8,24 @@ module.exports = {
   /* Your site config here */
   siteMetadata: {
     title: `Pandas Eating Lots`,
+    description: `A simple description about pandas eating lots...`,
+    author: `Flouse`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `static/favicon.ico`, // This path is relative to the root of the site.
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -24,6 +40,7 @@ module.exports = {
       options: {
         pathToConfigModule: `src/utils/typography`,
       },
-    }
+    },
+    `gatsby-plugin-react-helmet`,
   ]
 }
---
+++ src/pages/new-markdown-file.md
@@ -0,0 +1,12 @@
+---
+title: new for test
+date: 2021-04-17
+---
+
+# Challenge
+
+## Try playing more with the site. Try adding some more markdown files. Explore querying other data from the MarkdownRemark nodes and adding them to the front page or blog posts pages.
+
+### In this part of the tutorial, you’ve learned the foundations of building with Gatsby’s data layer. You’ve learned how to source and transform data using plugins, how to use GraphQL to map data to pages, and then how to build page template components where you query for data for each page.
+
+> /new-markdown-file/
